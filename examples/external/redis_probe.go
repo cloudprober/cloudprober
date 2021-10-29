6cf4dbd77d517f7f44e6e19a8afc2fde77c97414
Manu Garg <manugarg@gmail.com>
2021-10-29 00:14:23 -0700

    Update redis_probe.go

--- examples/external/redis_probe.go
+++ examples/external/redis_probe.go
@@ -48,8 +48,8 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
-	epb "github.com/google/cloudprober/probes/external/proto"
-	"github.com/google/cloudprober/probes/external/serverutils"
+	epb "github.com/cloudprober/cloudprober/probes/external/proto"
+	"github.com/cloudprober/cloudprober/probes/external/serverutils"
 	"github.com/hoisie/redis"
 )
 
