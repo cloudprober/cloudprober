33865469abd1b39386b4738464fa07d1d7f5747f
cstahl1 <cstahl1@users.noreply.github.com>
2021-12-06 13:51:28 -0500

    Allow multiple instances of cloudprober to run sequentially. (#33)
    * Allow multiple instances of cloudprober to run sequentially.
    
    This is most useful for tests in which a full cloudprober is required. Each
    test initializes a new instance, calling InitFromConfig and Start and when
    the context passed to Start is done, the cloud prober instance will shut
    down fully. There is currently no good way to ensure that the last instance
    has shut down before starting a new one, so I used a time.Sleep.
    
    - Added ClearDefaultGRPCServer() to runconfig to clear the saved gRPC
    server
    - On context close, Start will fully clean up the global variables found
    in cloudprober.go
    - Added test that starts CloudProber, stops it, and starts it again.
    Before this code change, that would have always failed with the second
    config ignored.

--- cloudprober_test.go
+++ cloudprober_test.go
@@ -15,10 +15,22 @@
 package cloudprober
 
 import (
+	"context"
+	"net"
 	"os"
 	"testing"
+	"time"
 
 	configpb "github.com/cloudprober/cloudprober/config/proto"
+	"github.com/cloudprober/cloudprober/metrics"
+	probepb "github.com/cloudprober/cloudprober/probes/proto"
+	udpprobepb "github.com/cloudprober/cloudprober/probes/udp/proto"
+	serverspb "github.com/cloudprober/cloudprober/servers/proto"
+	udpserverpb "github.com/cloudprober/cloudprober/servers/udp/proto"
+	"github.com/cloudprober/cloudprober/surfacers"
+	surfacerspb "github.com/cloudprober/cloudprober/surfacers/proto"
+	targetspb "github.com/cloudprober/cloudprober/targets/proto"
+	"google.golang.org/protobuf/encoding/prototext"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -83,4 +95,120 @@ func TestGetDefaultServerPort(t *testing.T) {
 		})
 	}
 
+}
+
+type FakeSurfacer struct {
+	c chan *metrics.EventMetrics
+}
+
+func (f *FakeSurfacer) Write(ctx context.Context, em *metrics.EventMetrics) {
+	if em.Label("ptype") != "udp" {
+		return
+	}
+	select {
+	case f.c <- em:
+	case <-ctx.Done():
+	}
+}
+
+func freePortsT(t *testing.T, n int) []int32 {
+	ports := make([]int32, 0, n)
+	for i := 0; i < n; i++ {
+		l, err := net.Listen("tcp", ":0")
+		if err != nil {
+			t.Fatalf("net.Listen(%q, %q): %v", "tcp", ":0", err)
+		}
+		defer l.Close()
+		ports = append(ports, int32(l.Addr().(*net.TCPAddr).Port))
+	}
+	return ports
+}
+
+func TestRestart(t *testing.T) {
+	type TestCase struct {
+		Name string
+	}
+	testCases := []TestCase{
+		{
+			Name: "FirstInitAndStart",
+		},
+		{
+			Name: "SecondInitAndStart",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer func() {
+				cancel()
+				// Wait required for the cloudprober instance to fully shut down.
+				time.Sleep(time.Second)
+			}()
+			ports := freePortsT(t, 3)
+			cfg := &configpb.ProberConfig{
+				Port:     proto.Int32(ports[0]),
+				GrpcPort: proto.Int32(ports[1]),
+				Server: []*serverspb.ServerDef{
+					{
+						Type: serverspb.ServerDef_UDP.Enum(),
+						Server: &serverspb.ServerDef_UdpServer{
+							UdpServer: &udpserverpb.ServerConf{
+								Port: proto.Int32(ports[2]),
+								Type: udpserverpb.ServerConf_ECHO.Enum(),
+							},
+						},
+					},
+				},
+				Probe: []*probepb.ProbeDef{
+					{
+						Name:                    proto.String("udp echo"),
+						Type:                    probepb.ProbeDef_UDP.Enum(),
+						TimeoutMsec:             proto.Int32(10),
+						IntervalMsec:            proto.Int32(10), // 100 probe per second
+						StatsExportIntervalMsec: proto.Int32(int32(1 * time.Second / time.Millisecond)),
+						Targets: &targetspb.TargetsDef{
+							Type: &targetspb.TargetsDef_HostNames{
+								HostNames: "localhost",
+							},
+						},
+						Probe: &probepb.ProbeDef_UdpProbe{
+							UdpProbe: &udpprobepb.ProbeConf{
+								Port:        proto.Int32(ports[2]),
+								PayloadSize: proto.Int32(10),
+							},
+						},
+					},
+				},
+			}
+			surfacerName := "custom"
+			cfg.Surfacer = []*surfacerspb.SurfacerDef{
+				&surfacerspb.SurfacerDef{
+					Name: proto.String(surfacerName),
+					Type: surfacerspb.Type_USER_DEFINED.Enum(),
+				},
+			}
+			s := &FakeSurfacer{c: make(chan *metrics.EventMetrics, 10)}
+			surfacers.Register(surfacerName, s)
+
+			b, err := prototext.Marshal(cfg)
+			if err != nil {
+				t.Fatalf("prototext.Marshal(%v): %v", cfg, err)
+			}
+
+			err = InitFromConfig(string(b))
+			if err != nil {
+				t.Fatalf("InitFromConfig(ctx, %v): %v", string(b), err)
+			}
+			Start(ctx)
+
+			// Wait for results from the surfacer, or 30s,
+			// whichever comes first. Since the export rate is 1s,
+			// we should expect results well before 30s has passed.
+			select {
+			case <-time.After(time.Second * 30):
+				t.Fatal("surfacer timed out before getting results")
+			case <-s.c:
+			}
+		})
+	}
 }
