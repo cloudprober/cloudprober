2d78dfad25f382a1e4a0ecd0066a533ed9089c8e
Manu Garg <manugarg@gmail.com>
2022-03-16 11:38:25 -0700

    [probestatus] Minor refactoring to improve readability.

--- surfacers/probestatus/probestatus_test.go
+++ surfacers/probestatus/probestatus_test.go
@@ -16,7 +16,6 @@ package probestatus
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 	"time"
 
@@ -44,41 +43,44 @@ func TestRecord(t *testing.T) {
 	total, success := 200, 180
 	latency := float64(2000)
 	inputInterval := 30 * time.Second
+	testDurations := []time.Duration{time.Minute, 2 * time.Minute, 5 * time.Minute, 10 * time.Minute}
 
 	var tests = []struct {
 		numMetrics  int
 		latest      int
 		oldest      int
 		oldestTotal int
+		lenData     int     // lenght of data for 10m query.
 		totalDeltas []int64 // for durations 1, 2, 5, 10 min
 	}{
 		{
 			// _, [1], [2,3], [4,5], [6,7], [8,9], [10, 11], [12, 13]
 			numMetrics:  13,
 			oldest:      0,
-			latest:      7, // Leave first empty, fill next 7
-			oldestTotal: 187,
-			totalDeltas: []int64{2, 4, 10, 12}, // 7 buckets max.
+			latest:      7,   // Leave first empty, fill next 7
+			oldestTotal: 200, // First input
+			lenData:     7,   // First bucket empty.
+			totalDeltas: []int64{2, 4, 10, 12},
 		},
 		{
 			// _, [1, 2], [3,4], [5,6], [7,8], [9,10], [11,12], [13,14],
 			//                                 ........[15,16], [17,18]
 			numMetrics:  18,
 			oldest:      0,
-			latest:      9,                     // Leave first empty, fill next 9
-			oldestTotal: 183,                   // First gets overwritten
-			totalDeltas: []int64{2, 4, 10, 16}, // 9 buckets for 10m window.
+			latest:      9,   // Leave first empty, fill next 9
+			oldestTotal: 201, // First gets overwritten
+			lenData:     9,   // First bucket is empty.
+			totalDeltas: []int64{2, 4, 10, 16},
 		},
 		{
 			// [19,20], [21,22], [3,4], [5,6], [7,8], [9,10], [11,12], [13,14],
 			//                                            ... [15,16], [17,18]
-			numMetrics: 22, // It will rotate.
-			oldest:     2,
-			latest:     1,
-
-			// Start at 178, but [1,2] is lost, and 3 is overwritten by 4.
-			oldestTotal: 181,
-			totalDeltas: []int64{2, 4, 10, 18}, // 10 buckets for 10m window.
+			numMetrics:  22, // It will rotate.
+			oldest:      2,
+			latest:      1,
+			oldestTotal: 203, // First bucket is lost
+			lenData:     10,
+			totalDeltas: []int64{2, 4, 10, 18},
 		},
 	}
 
@@ -96,10 +98,11 @@ func TestRecord(t *testing.T) {
 				l:            &logger.Logger{},
 			}
 
-			for i := test.numMetrics; i >= 1; i-- {
-				tm := time.Now().Truncate(time.Minute).Add(time.Duration(-i) * inputInterval)
-				ps.record(testEM(t, tm, "t1", total-i, success-i, latency-9.9*float64(i)))
-				ps.record(testEM(t, tm, "t2", total-i, success-2*i, latency-10*float64(i)))
+			// Start test.numMetrics ago.
+			baseTime := time.Now().Truncate(time.Minute).Add(time.Duration(-test.numMetrics) * inputInterval)
+			for i := 0; i < test.numMetrics; i++ {
+				ps.record(testEM(t, baseTime.Add(time.Duration(i)*inputInterval), "t1", total+i, success+i, latency+9.9*float64(i)))
+				ps.record(testEM(t, baseTime.Add(time.Duration(i)*inputInterval), "t2", total+i, success+i, latency+10*float64(i)))
 			}
 
 			for _, target := range []string{"t1", "t2"} {
@@ -134,12 +137,16 @@ func TestRecord(t *testing.T) {
 				})
 			}
 
-			durations := []time.Duration{time.Minute, 2 * time.Minute, 5 * time.Minute, 10 * time.Minute}
 			data := ts.getRecentData(10 * time.Minute)
-			totalDeltas, _ := ps.computeDelta(durations, data)
-			t.Logf("%+v", len(data))
-			if !reflect.DeepEqual(totalDeltas, test.totalDeltas) {
-				t.Errorf("total deltas=%v, want deltas=%v", totalDeltas, test.totalDeltas)
+			if len(data) != test.lenData {
+				t.Errorf("len(data)=%d, wanted=%d", len(data), test.lenData)
+			}
+
+			for i, td := range testDurations {
+				totalDelta, _ := ps.computeDelta(data, td)
+				if totalDelta != test.totalDeltas[i] {
+					t.Errorf("total delta for duration (%s)=%v, wanted=%v", td, totalDelta, test.totalDeltas[i])
+				}
 			}
 		})
 	}
