46b24d2f1827d6b9ae90c2d3fbe53751fc21457c
Manu Garg <manugarg@gmail.com>
2022-03-19 12:36:02 -0700

    [probestatus] Minor style changes.
    = Include max duration in dashboard if not included already.
    = Show 1m success ratio as well.
    = Cache only for 2s (instead of 5s).

--- surfacers/probestatus/dashboard.go
+++ surfacers/probestatus/dashboard.go
@@ -15,44 +15,50 @@
 package probestatus
 
 import (
+	"fmt"
 	"strings"
 	"time"
 )
 
 var baseDurations = []time.Duration{
-	05 * time.Minute, 30 * time.Minute, 60 * time.Minute,
-	06 * time.Hour, 12 * time.Hour, 24 * time.Hour,
-	72 * time.Hour, 168 * time.Hour, 720 * time.Hour,
+	01 * time.Minute, 05 * time.Minute, 30 * time.Minute, 60 * time.Minute, // Minutes
+	06 * time.Hour, 24 * time.Hour, // Hours
+	72 * time.Hour, 168 * time.Hour, 720 * time.Hour, // Days
 }
 
-var hourToDayMapping = map[string]string{
-	"72h":  "3d",
-	"168h": "7d",
-	"720h": "30d",
-}
-
-func dashboardDurations(maxDuration time.Duration) []time.Duration {
+func dashboardDurations(maxDuration time.Duration) ([]time.Duration, []string) {
 	var result []time.Duration
 	for _, td := range baseDurations {
 		if td <= maxDuration {
 			result = append(result, td)
 		}
 	}
-	return result
+	if result[len(result)-1] != maxDuration {
+		result = append(result, maxDuration)
+	}
+	return result, shortDur(result)
 }
 
 func shortDur(durations []time.Duration) []string {
 	var result []string
 	for _, td := range durations {
+		// Convert days first.
+		if td.Truncate(24*time.Hour) == td {
+			days := td / (24 * time.Hour)
+			if days == 1 {
+				result = append(result, "24h")
+				continue
+			}
+			result = append(result, fmt.Sprintf("%dd", days))
+			continue
+		}
+
 		s := td.String()
 		if strings.HasSuffix(s, "m0s") {
 			s = s[:len(s)-2]
 		}
 		if strings.HasSuffix(s, "h0m") {
 			s = s[:len(s)-2]
-		}
-		if d, ok := hourToDayMapping[s]; ok {
-			s = d
 		}
 		result = append(result, s)
 	}
--- surfacers/probestatus/dashboard_test.go
+++ surfacers/probestatus/dashboard_test.go
@@ -25,12 +25,13 @@ func TestDashboardDurations(t *testing.T) {
 		24 * time.Hour:  baseDurations[:6],
 		72 * time.Hour:  baseDurations[:7],
 		168 * time.Hour: baseDurations[:8],
+		360 * time.Hour: append(append([]time.Duration{}, baseDurations[:8]...), 360*time.Hour),
 		720 * time.Hour: baseDurations,
 	}
 
 	for maxD, wantDurations := range testData {
 		t.Run(maxD.String(), func(t *testing.T) {
-			durations := dashboardDurations(maxD)
+			durations, _ := dashboardDurations(maxD)
 			if !reflect.DeepEqual(durations, wantDurations) {
 				t.Errorf("Got durations=%v, wanted=%v", durations, wantDurations)
 			}
@@ -43,6 +44,7 @@ func TestShortDur(t *testing.T) {
 		30 * time.Minute: "30m",
 		24 * time.Hour:   "24h",
 		72 * time.Hour:   "3d",
+		96 * time.Hour:   "4d",
 		168 * time.Hour:  "7d",
 		720 * time.Hour:  "30d",
 	}
