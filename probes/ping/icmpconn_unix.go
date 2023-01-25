e497e0230f348593895e986f9465780ae6c7e8d0
Darin Peetz <darinpeetz@google.com>
2023-01-25 14:31:49 -0600

    Restore the accidentally removed build tag for darwin.

--- probes/ping/icmpconn_unix.go
+++ probes/ping/icmpconn_unix.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build aix || dragonfly || freebsd || netbsd || openbsd || solaris
-// +build aix dragonfly freebsd netbsd openbsd solaris
+//go:build aix || darwin || dragonfly || freebsd || netbsd || openbsd || solaris
+// +build aix darwin dragonfly freebsd netbsd openbsd solaris
 
 package ping
 
