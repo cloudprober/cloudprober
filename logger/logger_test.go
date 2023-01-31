e5fb26152cf65c86563ce12dd47f94e92dbb1bd3
Manu Garg <manugarg@gmail.com>
2023-01-30 23:49:12 -0800

    [logger] WithLabels should just add labels, and add tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,9 +1,12 @@
 package logger
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEnvVarSet(t *testing.T) {
@@ -31,6 +34,41 @@ func TestEnvVarSet(t *testing.T) {
 			if got != row.expected {
 				t.Errorf("Variable set: got=%v, expected=%v", got, row.expected)
 			}
+		})
+	}
+}
+
+func TestWithLabels(t *testing.T) {
+	panicOnErr := func(l *Logger, err error) *Logger {
+		if err != nil {
+			panic(err)
+		}
+		return l
+	}
+	tests := []struct {
+		l          *Logger
+		wantLabels map[string]string
+	}{
+		{
+			l:          panicOnErr(New(context.Background(), "newWithLabels", WithLabels(map[string]string{"k1": "v1"}))),
+			wantLabels: map[string]string{"k1": "v1"},
+		},
+		{
+			l:          panicOnErr(New(context.Background(), "newWithLabels", WithLabels(map[string]string{"k1": "v1"}), WithLabels(map[string]string{"k1": "v2"}))),
+			wantLabels: map[string]string{"k1": "v2"},
+		},
+		{
+			l: func() *Logger {
+				l := &Logger{}
+				WithLabels(map[string]string{"k1": "v1", "k3": "v3"})(l)
+				return l
+			}(),
+			wantLabels: map[string]string{"k1": "v1", "k3": "v3"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.l.name, func(t *testing.T) {
+			assert.Equal(t, tt.wantLabels, tt.l.labels)
 		})
 	}
 }
