1609ab609a3dd96858e96687ae3a798539228cf9
Manu Garg <manugarg@gmail.com>
2023-05-03 17:18:53 -0700

    [http] Move HTTP request creation to a common package.

--- common/httputils/httputils.go
+++ common/httputils/httputils.go
@@ -16,11 +16,66 @@
 package httputils
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 )
 
 func IsHandled(mux *http.ServeMux, url string) bool {
 	_, matchedPattern := mux.Handler(httptest.NewRequest("", url, nil))
 	return matchedPattern == url
+}
+
+// RequestBody encapsulates the request body and implements the io.Reader()
+// interface.
+type RequestBody struct {
+	b []byte
+}
+
+// NewRequestBody returns a new RequestBody object.
+func NewRequestBody(data []byte) *RequestBody {
+	return &RequestBody{b: data}
+}
+
+// Read implements the io.Reader interface. Instead of using buffered read,
+// it simply copies the bytes to the provided slice in one go (depending on
+// the input slice capacity) and returns io.EOF. Buffered reads require
+// resetting the buffer before re-use, restricting our ability to reuse the
+// request object and using it concurrently.
+func (rb *RequestBody) Read(p []byte) (int, error) {
+	return copy(p, rb.b), io.EOF
+}
+
+func setContentType(req *http.Request, data []string) {
+	if len(data) == 1 {
+		if json.Valid([]byte(data[0])) {
+			req.Header.Set("Content-Type", "application/json")
+			return
+		}
+	}
+	if _, err := url.ParseQuery(strings.Join(data, "&")); err == nil {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		return
+	}
+}
+
+func HTTPRequest(method, url string, data []string, headers map[string]string) (*http.Request, error) {
+	body := &RequestBody{b: []byte(strings.Join(data, "&"))}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("invalid config: %v", err)
+	}
+
+	setContentType(req, data)
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	return req, nil
 }
