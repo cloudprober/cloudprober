10841b1d9f3bc191674be5bcc5996627b2f71280
Manu Garg <manugarg@gmail.com>
2023-07-31 14:24:29 -0700

    Add test for cached reading

--- common/file/file_test.go
+++ common/file/file_test.go
@@ -16,7 +16,11 @@ package file
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func createTempFile(t *testing.T, b []byte) string {
@@ -65,4 +69,42 @@ func TestReadFile(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestReadWithCache(t *testing.T) {
+	f, err := os.CreateTemp("", "")
+	if err != nil {
+		t.Fatalf("Error creating temp file: %v", err)
+	}
+	defer os.Remove(f.Name())
+
+	writeToTempFile := func(s string) {
+		if _, err := f.WriteAt([]byte(s), 0); err != nil {
+			t.Fatalf("Error writing to temp file (%s): %v", f.Name(), err)
+		}
+	}
+
+	readAndVerify := func(expectedContent string, reloadInterval time.Duration) {
+		b, err := ReadWithCache(f.Name(), reloadInterval)
+		assert.NoError(t, err, "reading file")
+		assert.Equal(t, expectedContent, string(b))
+	}
+
+	testContent := "test-content"
+	writeToTempFile(testContent)
+
+	// No cache
+	readAndVerify(testContent, 0)
+	writeToTempFile(testContent + "-updated")
+	readAndVerify(testContent+"-updated", 0)
+
+	// with cache
+	writeToTempFile(testContent + "-updated-2")
+	// we see old content before cache expiration.
+	// Note: we use 2 seconds here to make sure that the test doesn't fail due
+	// to timing issues.
+	readAndVerify(testContent+"-updated", 2*time.Second)
+	// wait for cache to expire
+	time.Sleep(time.Second)
+	readAndVerify(testContent+"-updated-2", 1*time.Second)
 }
