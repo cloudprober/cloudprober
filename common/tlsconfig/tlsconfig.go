c686c0c1aa6376766ac847e1dda757a0bc338038
Manu Garg <manugarg@gmail.com>
2023-08-01 16:52:02 -0700

    [tls] Provide an option to reload TLS certificates. (#428)
    * Add a bunch of tests to cover new and old functionality.

--- common/tlsconfig/tlsconfig.go
+++ common/tlsconfig/tlsconfig.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Cloudprober Authors.
+// Copyright 2019-2023 The Cloudprober Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,10 +19,33 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"time"
 
 	"github.com/cloudprober/cloudprober/common/file"
 	configpb "github.com/cloudprober/cloudprober/common/tlsconfig/proto"
 )
+
+func loadCert(certFile, keyFile string, refreshInterval time.Duration) (*tls.Certificate, error) {
+	readFile := file.ReadFile
+	if refreshInterval > 0 {
+		readFile = func(filename string) ([]byte, error) {
+			return file.ReadWithCache(filename, refreshInterval)
+		}
+	}
+
+	certPEMBlock, err := readFile(certFile)
+	if err != nil {
+		return nil, fmt.Errorf("common/tlsconfig: error reading TLS cert file (%s): %v", certFile, err)
+	}
+
+	keyPEMBlock, err := readFile(keyFile)
+	if err != nil {
+		return nil, fmt.Errorf("common/tlsconfig: error reading TLS key file (%s): %v", keyFile, err)
+	}
+
+	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
+	return &cert, err
+}
 
 // UpdateTLSConfig parses the provided protobuf and updates the tls.Config object.
 func UpdateTLSConfig(tlsConfig *tls.Config, c *configpb.TLSConfig) error {
@@ -44,20 +67,21 @@ func UpdateTLSConfig(tlsConfig *tls.Config, c *configpb.TLSConfig) error {
 	}
 
 	if c.GetTlsCertFile() != "" {
-		certPEMBlock, err := file.ReadFile(c.GetTlsCertFile())
+		// Even if we are live-reloading the cert, verify early that we can
+		// load the certificate.
+		cert, err := loadCert(c.GetTlsCertFile(), c.GetTlsKeyFile(), 0)
 		if err != nil {
-			return fmt.Errorf("common/tlsconfig: error reading TLS cert file (%s): %v", c.GetTlsCertFile(), err)
-		}
-		keyPEMBlock, err := file.ReadFile(c.GetTlsKeyFile())
-		if err != nil {
-			return fmt.Errorf("common/tlsconfig: error reading TLS key file (%s): %v", c.GetTlsKeyFile(), err)
+			return err
 		}
 
-		cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
-		if err != nil {
-			return fmt.Errorf("common/tlsconfig: error initializing cert from cert key pair: %v", err)
+		if c.GetReloadIntervalSec() > 0 {
+			tlsConfig.GetCertificate = func(_ *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				return loadCert(c.GetTlsCertFile(), c.GetTlsKeyFile(), time.Duration(c.GetReloadIntervalSec())*time.Second)
+			}
+			return nil
 		}
-		tlsConfig.Certificates = append(tlsConfig.Certificates, cert)
+
+		tlsConfig.Certificates = append(tlsConfig.Certificates, *cert)
 	}
 
 	if c.GetServerName() != "" {
