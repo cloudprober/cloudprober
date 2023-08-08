ee3439897667de7b4504a76a06f7b7152c2f21ed
Manu Garg <manugarg@gmail.com>
2023-08-08 00:13:12 -0700

    Fix grpc probe

--- probes/grpc/grpc.go
+++ probes/grpc/grpc.go
@@ -338,7 +338,7 @@ func (p *Probe) oneTargetLoop(ctx context.Context, tgt endpoint.Endpoint, index 
 		reqCtx, cancelFunc := context.WithTimeout(ctx, timeout)
 		reqCtx = p.ctxWithHeaders(reqCtx)
 
-		var success int64
+		var success bool
 		var delta time.Duration
 		start := time.Now()
 		var err error
@@ -376,13 +376,15 @@ func (p *Probe) oneTargetLoop(ctx context.Context, tgt endpoint.Endpoint, index 
 			}
 			p.l.Warningf("ProbeId(%s) request failed: %v. ConnState: %v. Peer: %v", msgPattern, err, conn.GetState(), peerAddr)
 		} else {
-			success = 1
+			success = true
 			delta = time.Since(start)
 		}
 		// TODO(ls692): add validators for probe result.
 		result.Lock()
 		result.total.Inc()
-		result.success.AddInt64(success)
+		if success {
+			result.success.Inc()
+		}
 		result.latency.AddFloat64(delta.Seconds() / p.opts.LatencyUnit.Seconds())
 		result.Unlock()
 	}
