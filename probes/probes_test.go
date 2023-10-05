333e47630bfa0391f585da765e184bee64e3a49b
Rob Pickerill <r.pickerill@gmail.com>
2023-10-05 12:51:46 +0100

    add copywright, and note about the probe test files

--- probes/probes_test.go
+++ probes/probes_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Cloudprober Authors.
+// Copyright 2017-2023 The Cloudprober Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// This file is for external tests of the probe packages, created to provide
+// tests for the probe extensions.
 
 package probes_test
 
--- probes/probes_unit_test.go
+++ probes/probes_unit_test.go
@@ -1,3 +1,21 @@
+// Copyright 2017-2023 The Cloudprober Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file is for internal unit tests for probes package. It was
+// created to test the fix for the following issue:
+// https://github.com/cloudprober/cloudprober/issues/293
+
 package probes
 
 import (
@@ -9,6 +27,8 @@ import (
 
 // This test is to make sure that we don't panic on empty config.
 // https://github.com/cloudprober/cloudprober/issues/293
+// This test utilizes the `probes.initProbe` function, which is not exported by
+// the probes package.
 func TestProbesAllowEmptyConfig(t *testing.T) {
 	for probeName, probeValue := range configpb.ProbeDef_Type_value {
 		t.Run(probeName, func(t *testing.T) {
