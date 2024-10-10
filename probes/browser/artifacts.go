20f51af1be12f5660dfe582d4fea64f0dcd2ebad
Manu Garg <manugarg@gmail.com>
2024-10-10 11:00:40 -0700

    [probes.browser] Implement local artifacts storage (#885)
    - Factor out common code for walking through artifacts directory and add tests for it.
    - Add test for local store file saving.

--- probes/browser/artifacts.go
+++ probes/browser/artifacts.go
@@ -26,13 +26,16 @@ import (
 )
 
 type artifactsHandler struct {
-	basePath  string
-	s3Storage []*s3Storage
+	basePath     string
+	s3Storage    []*s3Storage
+	localStorage []*localStorage
+	l            *logger.Logger
 }
 
 func initArtifactsHandler(opts *configpb.ArtifactsOptions, probeName, outputDir string, l *logger.Logger) (*artifactsHandler, error) {
 	ah := &artifactsHandler{
 		basePath: outputDir,
+		l:        l,
 	}
 
 	if opts.GetServeOnWeb() {
@@ -52,26 +55,43 @@ func initArtifactsHandler(opts *configpb.ArtifactsOptions, probeName, outputDir 
 
 	for _, storageConfig := range opts.GetStorage() {
 		if s3conf := storageConfig.GetS3(); s3conf != nil {
-			s3, err := initS3(context.Background(), s3conf, l)
+			s3, err := initS3(context.Background(), s3conf)
 
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("error initializing S3 storage (bucket: %s): %v", s3conf.GetBucket(), err)
 			}
 
 			ah.s3Storage = append(ah.s3Storage, s3)
+		}
+
+		if dir := storageConfig.GetLocalStorageDir(); dir != "" {
+			ls, err := initLocalStorage(dir)
+			if err != nil {
+				return nil, fmt.Errorf("error initializing local storage: %v", err)
+			}
+			ah.localStorage = append(ah.localStorage, ls)
 		}
 	}
 
 	return ah, nil
 }
 
-func (ah *artifactsHandler) handle(path string) {
+func (ah *artifactsHandler) handle(ctx context.Context, path string) {
 	for _, s3 := range ah.s3Storage {
-		s3 := s3 // create a new variable to avoid capturing the loop variable
 		go func(s3 *s3Storage) {
-			if err := s3.uploadDirToS3(context.Background(), path, ah.basePath); err != nil {
-				s3.l.Errorf("error uploading artifacts to S3: %v", err)
+			ah.l.Infof("Uploading artifacts from %s to: s3://%s/%s", path, s3.bucket, s3.path)
+			if err := s3.store(ctx, path, ah.basePath); err != nil {
+				ah.l.Errorf("error uploading artifacts to S3: %v", err)
 			}
 		}(s3)
+	}
+
+	for _, lStorage := range ah.localStorage {
+		go func(lStorage *localStorage) {
+			ah.l.Infof("Saving artifacts from %s at: %s", path, lStorage.destDir)
+			if err := lStorage.store(ctx, path, ah.basePath); err != nil {
+				ah.l.Errorf("error saving artifacts locally: %v", err)
+			}
+		}(lStorage)
 	}
 }
