4b807e65ef8f3ecedfdff4a5d1e59b92087649a9
Manu Garg <manugarg@gmail.com>
2024-10-17 22:33:16 -0700

    [probes.browser] Add support for uploading artifacts to GCS (#891)
    - Tested with a GCP account.

---
+++ probes/browser/artifacts.go
@@ -0,0 +1,97 @@
+// Copyright 2024 The Cloudprober Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package browser
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/cloudprober/cloudprober/config/runconfig"
+	"github.com/cloudprober/cloudprober/logger"
+	configpb "github.com/cloudprober/cloudprober/probes/browser/proto"
+)
+
+type artifactsHandler struct {
+	basePath     string
+	s3Storage    []*s3Storage
+	localStorage []*localStorage
+	l            *logger.Logger
+}
+
+func initArtifactsHandler(opts *configpb.ArtifactsOptions, probeName, outputDir string, l *logger.Logger) (*artifactsHandler, error) {
+	ah := &artifactsHandler{
+		basePath: outputDir,
+		l:        l,
+	}
+
+	if opts.GetServeOnWeb() {
+		httpServerMux := runconfig.DefaultHTTPServeMux()
+		if httpServerMux == nil {
+			return nil, fmt.Errorf("default http server mux is not initialized")
+		}
+
+		pathPrefix := opts.GetWebServerPath()
+		if pathPrefix == "" {
+			pathPrefix = filepath.Join("/artifacts", probeName)
+		}
+
+		fileServer := http.FileServer(http.Dir(outputDir))
+		httpServerMux.Handle(pathPrefix+"/", http.StripPrefix(pathPrefix, fileServer))
+	}
+
+	for _, storageConfig := range opts.GetStorage() {
+		if s3conf := storageConfig.GetS3(); s3conf != nil {
+			s3, err := initS3(context.Background(), s3conf)
+
+			if err != nil {
+				return nil, fmt.Errorf("error initializing S3 storage (bucket: %s): %v", s3conf.GetBucket(), err)
+			}
+
+			ah.s3Storage = append(ah.s3Storage, s3)
+		}
+
+		if dir := storageConfig.GetLocalStorageDir(); dir != "" {
+			ls, err := initLocalStorage(dir)
+			if err != nil {
+				return nil, fmt.Errorf("error initializing local storage: %v", err)
+			}
+			ah.localStorage = append(ah.localStorage, ls)
+		}
+	}
+
+	return ah, nil
+}
+
+func (ah *artifactsHandler) handle(ctx context.Context, path string) {
+	for _, s3 := range ah.s3Storage {
+		go func(s3 *s3Storage) {
+			ah.l.Infof("Uploading artifacts from %s to: s3://%s/%s", path, s3.bucket, s3.path)
+			if err := s3.store(ctx, path, ah.basePath); err != nil {
+				ah.l.Errorf("error uploading artifacts to S3: %v", err)
+			}
+		}(s3)
+	}
+
+	for _, lStorage := range ah.localStorage {
+		go func(lStorage *localStorage) {
+			ah.l.Infof("Saving artifacts from %s at: %s", path, lStorage.destDir)
+			if err := lStorage.store(ctx, path, ah.basePath); err != nil {
+				ah.l.Errorf("error saving artifacts locally: %v", err)
+			}
+		}(lStorage)
+	}
+}
---
+++ probes/browser/storage_gcs_test.go
@@ -0,0 +1,136 @@
+// Copyright 2024 The Cloudprober Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package browser
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"slices"
+	"strings"
+	"testing"
+
+	configpb "github.com/cloudprober/cloudprober/probes/browser/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGCSBaseURL(t *testing.T) {
+	tests := []struct {
+		endpoint string
+		bucket   string
+		want     string
+	}{
+		{
+			bucket: "cloudprober-browser",
+			want:   "https://storage.googleapis.com/upload/storage/v1/b/cloudprober-browser/o?uploadType=media&name=",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.want, func(t *testing.T) {
+			cfg := &configpb.GCS{
+				Bucket: &tt.bucket,
+			}
+			if tt.endpoint != "" {
+				cfg.Endpoint = &tt.endpoint
+			}
+			assert.Equal(t, tt.want, gcsBaseURL(cfg))
+		})
+	}
+}
+
+type fakeRoundTripper struct {
+	validObjectNames []string
+	wantPrefix       string
+}
+
+func (f *fakeRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	errorResp := &http.Response{
+		StatusCode: http.StatusInternalServerError,
+		Body:       io.NopCloser(bytes.NewBufferString("error")),
+	}
+	if !strings.HasPrefix(req.URL.String(), f.wantPrefix) {
+		return errorResp, nil
+	}
+
+	objectName := req.URL.Query().Get("name")
+	if !slices.Contains(f.validObjectNames, objectName) {
+		return errorResp, nil
+	}
+
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(bytes.NewBufferString("ok")),
+	}, nil
+}
+
+func TestGCSUpload(t *testing.T) {
+	relPaths := []string{
+		"2024-10-10/1234/test.png",
+		"/test.png",
+		"cause-error",
+	}
+	baseURL := "https://storage.googleapis.com/upload/storage/v1/b/test-bucket/o?uploadType=media&name="
+	client := &http.Client{
+		Transport: &fakeRoundTripper{
+			wantPrefix:       baseURL,
+			validObjectNames: relPaths[:2],
+		},
+	}
+
+	tests := []struct {
+		name     string
+		baseURL  string
+		relPaths []string
+		wantErr  bool
+	}{
+		{
+			name:     "valid",
+			baseURL:  baseURL,
+			relPaths: relPaths[:2],
+			wantErr:  false,
+		},
+		{
+			name:     "error-paths",
+			baseURL:  baseURL,
+			relPaths: relPaths[2:],
+			wantErr:  true,
+		},
+		{
+			name:     "error-url",
+			baseURL:  strings.Replace(baseURL, "storage", "storage-staging", 1),
+			relPaths: relPaths[:2],
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			storage := &gcsStorage{
+				client:  client,
+				baseURL: tt.baseURL,
+			}
+			for _, path := range tt.relPaths {
+				t.Run(path, func(t *testing.T) {
+					if tt.wantErr {
+						assert.Error(t, storage.upload(context.Background(), nil, path))
+					} else {
+						assert.NoError(t, storage.upload(context.Background(), nil, path))
+					}
+				})
+			}
+		})
+	}
+}
