14a43abe518bf2d74c8102ac2a85c1beea49a495
Manu Garg <manugarg@gmail.com>
2024-11-11 19:38:21 -0800

    Add an independent workflow for latest/release config docs

--- tools/gen_config_docs.sh
+++ tools/gen_config_docs.sh
@@ -7,6 +7,7 @@
 #   git checkout v0.13.7
 #   /tmp/gen_config_docs.sh v0.13.7
 DOCS_VERSION=$1
+RELEASE=$1
 
 if [ -z "${DOCS_VERSION}" ]; then
     DOCS_VERSION=$(git describe --exact-match --exclude tip --tags HEAD 2>/dev/null || /bin/true)
@@ -18,15 +19,34 @@ fi
 
 DOCS_VERSION=${DOCS_VERSION//\//_}
 
+ORIGINAL_DIR=$(pwd)
+
+if [ "${RELEASE}" == "latest" ]; then
+  RELEASE=$(curl -s https://api.github.com/repos/cloudprober/cloudprober/releases/latest | grep 'tag_name' | cut -d '"' -f4)
+fi
+
+if [ ! -z "${RELEASE}" ]; then
+  TEMPDIR=$(mktemp -d) && cd $TEMPDIR
+  wget https://github.com/cloudprober/cloudprober/archive/refs/tags/${RELEASE}.tar.gz
+  tar -xzf ${RELEASE}.tar.gz
+  mv cloudprober-${RELEASE/v/} cloudprober
+  cd cloudprober
+fi
+
 go install github.com/manugarg/protodoc/cmd/protodoc@latest
 protodoc --proto_root_dir=. --package_prefix=github.com/cloudprober/cloudprober \
-    --format=yaml --out_dir=docs/_config_docs/${DOCS_VERSION}/yaml --extra_msgs=cloudprober.rds.file.FileResources
+    --format=yaml --out_dir=${ORIGINAL_DIR}/docs/_config_docs/${DOCS_VERSION}/yaml --extra_msgs=cloudprober.rds.file.FileResources
 protodoc --proto_root_dir=. --package_prefix=github.com/cloudprober/cloudprober \
-    --format=textpb --out_dir=docs/_config_docs/${DOCS_VERSION}/textpb  --extra_msgs=cloudprober.rds.file.FileResources
-mkdir -p docs/content/docs/config/${DOCS_VERSION}
+    --format=textpb --out_dir=${ORIGINAL_DIR}/docs/_config_docs/${DOCS_VERSION}/textpb  --extra_msgs=cloudprober.rds.file.FileResources
 
-for dir in docs/_config_docs/${DOCS_VERSION}/yaml/*; do
-  cat > docs/content/docs/config/${DOCS_VERSION}/$(basename $dir).md <<EOF
+BASE_PATH=${ORIGINAL_DIR}/docs/content/docs/config/${DOCS_VERSION}
+mkdir -p ${BASE_PATH}
+
+for dir in ${ORIGINAL_DIR}/docs/_config_docs/${DOCS_VERSION}/textpb/*; do
+  if [ ! -d $dir ]; then
+    continue
+  fi
+  cat > ${BASE_PATH}/$(basename $dir).md <<EOF
 ---
 menu:
   docs:
