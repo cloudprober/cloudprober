a66a27cc684ae1dc9f0caa59ff31739606a0c1c7
Manu Garg <manugarg@gmail.com>
2025-03-04 13:59:10 -0800

    fix typo

--- surfacers/internal/probestatus/probestatus.go
+++ surfacers/internal/probestatus/probestatus.go
@@ -134,7 +134,7 @@ func New(ctx context.Context, config *configpb.SurfacerConf, opts *options.Optio
 	}
 
 	if config.GetUrl() != "/status" {
-		l.Warningf("Setting status page url is deprecated. In future versions, url will always fixed at /status and setting it to anything else will result in an error")
+		l.Warningf("Setting status page url is deprecated. In future versions, url will always be fixed at /status and setting it to anything else will result in an error")
 	}
 
 	ps := &Surfacer{
