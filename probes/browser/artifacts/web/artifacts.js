f490b7a652ea84e7c344534967e9aeea42dd04cd
Manu Garg <manugarg@gmail.com>
2025-05-28 17:48:09 -0700

    [probes.browser] Show timezone in datetime selector labels.

--- probes/browser/artifacts/web/artifacts.js
+++ probes/browser/artifacts/web/artifacts.js
@@ -29,7 +29,21 @@ function stringToTime(s) {
     }
     return new Date(n);
 }
-    
+
+function getLocalTimeZoneAbbreviation() {
+    const date = new Date();
+    const timeZoneName = new Intl.DateTimeFormat("en-US", { timeZoneName: "short" })
+      .formatToParts(date)
+      .find((part) => part.type === "timeZoneName")
+      .value;
+    return timeZoneName;
+}
+
+const timeZoneAbbrElements = document.getElementsByClassName('time-zone-abbr');
+for (let i = 0; i < timeZoneAbbrElements.length; i++) {
+    timeZoneAbbrElements[i].textContent = getLocalTimeZoneAbbreviation();
+}
+
 const startDatetimeInput = document.getElementById('start-datetime');
 const endDatetimeInput = document.getElementById('end-datetime');
 const errorMessage = document.getElementById('error-message');
